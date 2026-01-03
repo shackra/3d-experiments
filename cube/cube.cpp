deb95a7fe3ea86bb572012469f9f577921b51a23
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-01-03 16:39:35 -0600

    Tell clang-tidy to ignore some magic numbers

--- cube/cube.cpp
+++ cube/cube.cpp
@@ -1,14 +1,13 @@
-#include "Ogre.h"
-#include "OgreApplicationContext.h"
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
 
-int main(int argc, char *argv[]) {
+auto main() -> int {
   OgreBites::ApplicationContext ctx("Cube");
   ctx.initApp();
 
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
       // TODO: find a way to make this directory always point to the right place
-      "cube/mesh/",
-      "FileSystem",
+      "cube/mesh/", "FileSystem",
       Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 
   Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
@@ -23,14 +22,17 @@ int main(int argc, char *argv[]) {
   Ogre::Light *light = scnMgr->createLight("MainLight");
   Ogre::SceneNode *lightNode =
       scnMgr->getRootSceneNode()->createChildSceneNode();
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   lightNode->setPosition(0, 10, 15);
   lightNode->attachObject(light);
 
   Ogre::SceneNode *camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   camNode->setPosition(0, 0, 15);
   camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
 
   Ogre::Camera *cam = scnMgr->createCamera("myCam");
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   cam->setNearClipDistance(5);
   cam->setAutoAspectRatio(true);
   camNode->attachObject(cam);
@@ -39,7 +41,9 @@ int main(int argc, char *argv[]) {
 
   Ogre::Entity *ent = scnMgr->createEntity("Cube.mesh");
   Ogre::SceneNode *node = scnMgr->getRootSceneNode()->createChildSceneNode();
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   node->pitch(Ogre::Degree(-45));
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
   node->yaw(Ogre::Degree(-15));
   node->attachObject(ent);
 
